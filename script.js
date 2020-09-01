7b9897f6a21a55e09d34e4be1d7cec38ce7e874e
AllanGamal <63316423+AllanGamal@users.noreply.github.com>
2020-09-01 19:49:43 +0200

    more data structures

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
 let colors = ["#264653", "#008D8D", "#607D8B", "#D06045"];
 let nonColors = [];
 let teams = [];
+
+function addTeam(divId) {
+  let newTeam = { teamId: divId, fitters: [], stations: [] };
+  teams.push(newTeam);
+
+  // Color team card
+  // Add random color to team car
+  let n = Math.floor(Math.random() * (4 - nonColors.length));
+  document.getElementById(divId).style.backgroundColor = colors[n];
+  let test = divId.toString();
+
+  addElement("div", divId, "team__title", divId + "title-box");
+  addElement("h1", divId + "title-box", "teamtitle", divId + "-title");
+  document.getElementById(divId + "-title").innerHTML = divId;
+}
+
 // Add element function
-function addElement(parent, Class, id) {
-  let newDiv = document.createElement("div");
+function addElement(box, parent, Class, id) {
+  let newDiv = document.createElement(box);
   newDiv.classList = Class;
-  document.querySelector(parent).appendChild(newDiv);
+  document.getElementById(parent).appendChild(newDiv);
+  newDiv.id = id;
 
   // if div class == team, give team name and add to teams list
   if (Class == "team") {
-    newDiv.id = id;
-    let newTeam = { teamName: id, fitters: [], stations: [] };
-    teams.push(newTeam);
-
-    console.log(teams);
-
-    // Color team card
-    // Add random color to team car
-    let n = Math.floor(Math.random() * (4 - nonColors.length));
-    document.querySelector("#" + id).style.backgroundColor = colors[n];
+    addTeam(id);
   }
 }
+
+let asses = [0, 45, 687, 654];
 
 /////// Click button to add team
 document.getElementById("add-team").addEventListener("click", function () {
   let teamName = prompt("Team name");
-  if (teams.includes(teamName)) {
-    alert("Name already taken, try another one.");
+
+  if (teams.length > 0) {
+    for (let i = 0; i < teams.length; i++) {
+      let test = true;
+      if (teamName == teams[i].teamId) {
+        alert("Name already taken, try another one.");
+        continue;
+      } else {
+        if (teamName) {
+          addElement("div", "section", "team", teamName);
+        }
+      }
+      break;
+    }
   } else {
     if (teamName) {
-      addElement("section", "team", teamName);
+      addElement("div", "section", "team", teamName);
     }
   }
 });
